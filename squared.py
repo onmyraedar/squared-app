46dff30863d0b8f1994b14bd917a07b9c9189a5f
Reanna Ishmael <rishmael@fordham.edu>
2021-11-18 13:54:04 -0500

    Created Square Client

--- squared.py
+++ squared.py
@@ -1,1 +1,21 @@
-print("Testing new repo!")
+import os
+
+from dotenv import load_dotenv
+from square.client import Client
+
+load_dotenv()       # Takes environment variables from .env
+
+access_token = os.getenv("REPLACE_ACCESS_TOKEN")
+environment = os.getenv("SQ_ENVIRONMENT")
+
+client = Client(
+    access_token=access_token,
+    environment=environment
+)
+
+result = client.customers.list_customers()
+
+if result.is_success():
+  print(result.body)
+elif result.is_error():
+  print(result.errors)
