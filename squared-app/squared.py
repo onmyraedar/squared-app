bb6cbae7faa60489cdfac95b7b9db1a65e8ef1b6
Reanna Ishmael <rishmael@fordham.edu>
2021-11-19 17:45:04 -0500

    Updating templates

--- squared-app/squared.py
+++ squared-app/squared.py
@@ -1,7 +1,12 @@
 import os
 
 from dotenv import load_dotenv
+from flask import Flask, render_template, url_for
 from square.client import Client
+
+app = Flask(__name__)
+secret_key = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = secret_key
 
 load_dotenv()       # Takes environment variables from .env
 
@@ -13,9 +18,25 @@ client = Client(
     environment=environment
 )
 
-result = client.customers.list_customers()
+result = client.merchants.list_merchants()
 
 if result.is_success():
   print(result.body)
 elif result.is_error():
   print(result.errors)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", title="Home")
+
+@app.route("/stores")
+def stores():
+    return render_template("stores.html", title="Stores")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html", title="Dashboard")
+
+if __name__ == '__main__':
+    app.run(debug=True)
